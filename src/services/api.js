7caee54cfe6eb496b45793c781c4faee5352cdbe
vratika <bawangadevratika@gmail.com>
2026-01-05 12:13:52 +0530

    fix: production api baseURL and frontend build

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://hospital-queue-system-fn5j.onrender.com/",
+  baseURL: "https://hospital-queue-system-fn5j.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
